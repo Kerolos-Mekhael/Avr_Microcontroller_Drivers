818d2d58550cabb8cca16e230d2d414d06e60158
Kerolos-Mekhael <kerolos.mekhael14@gmail.com>
2022-01-18 19:55:46 +0200

    added AVR Driver

--- MCAL/Interrupt/EXTI/EXTI_prog.c
+++ MCAL/Interrupt/EXTI/EXTI_prog.c
@@ -1,17 +1,40 @@
-#include "../../lib/BIT_MATH.h"
-#include "../../lib/STD_TYPES.h"
+#include "../../../lib/BIT_MATH.h"
+#include "../../../lib/STD_TYPES.h"
 #include "EXTI_register.h"
 #include "EXTI_interface.h"
 
+/* Enable external interrupt_0 */
 void EXTI0_vidEnable(void){
 	
 	/* Set Control */
-SET_BIT(MCUSR, MCUSR_ISC01);
-CLR_BIT(MCUSR, MCUSR_ISC00);
+
+	SET_BIT(MCUCR,MCUCR_ISC01);
+	CLR_BIT(MCUCR,MCUCR_ISC00);
 
    /* Enable interrupt */
-SET_BIT(GICR, GICR_INT0);
-	
+	SET_BIT(GICR,GICR_INT0);
 }
 
+/* Enable external interrupt_1 */
+void EXTI1_vidEnable(void){
+
+	/* Set Control */
+
+	SET_BIT(MCUCR,MCUCR_ISC11);
+	CLR_BIT(MCUCR,MCUCR_ISC10);
+
+	/* Enable interrupt */
+	SET_BIT(GICR,GICR_INT1);
+}
+
+/* Enable external interrupt_2 */
+void EXTI2_vidEnable(void){
+
+	/* Set Control */
+
+	CLR_BIT(MCUCSR,MCUSR_ISC2);
+
+	/* Enable interrupt */
+	SET_BIT(GICR,GICR_INT2);
+}
 
--- MCAL/Interrupt/GIE_prog.c
+++ MCAL/Interrupt/GIE_prog.c
@@ -3,6 +3,12 @@
 #include "GIE_register.h"
 #include "GIE_interface.h"
 
+/* Enable global interrupt */
 void GIE_vidSetBit(void){
 	SET_BIT(SREG, SREG_I_BIT);
+}
+
+/* Disable global interrupt */
+void GIE_vidCLRBit(void){
+	CLR_BIT(SREG, SREG_I_BIT);
 }
