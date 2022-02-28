15628a9dced6fd3952e9f20e128b4c228fb77081
Kerolos_Mekhael <kerolos.mekhael14@gmail.com>
2022-02-28 17:45:25 +0200

    modified LCD Driver

--- HAL/01-LCD/LCD_prog.c
+++ HAL/01-LCD/LCD_prog.c
@@ -7,32 +7,55 @@
 
 
 
-/* Write Data to LCD */
+/* Send Data to LCD */
 void LCD_vidSendData(u8 cop_u8Data){
-
+	u8 Data = 0;
 	DIO_u8SetPinValue(LCD_CONTROLPORT, LCD_RS_PIN, PinHigh); 	/* RS is ON */
-	DIO_u8SetPinValue(LCD_CONTROLPORT, LCD_RW_PIN, PinLow);	/* RW is OFF */
-	DIO_u8SetPortValue(LCD_DATAPORT, cop_u8Data);				/* send Data */
+	DIO_u8SetPinValue(LCD_CONTROLPORT, LCD_RW_PIN, PinLow);		/* RW is OFF */
+	/* send 4-MSB of Data */
+	Data = cop_u8Data & 0xf0;
+	DIO_u8SetPortValue(LCD_DATAPORT, Data);
+	DIO_u8SetPinValue(LCD_CONTROLPORT, LCD_E_PIN, PinHigh);		/* Enable is ON */
+	_delay_ms(2);		/* Delay for 2 ms */
+	DIO_u8SetPinValue(LCD_CONTROLPORT, LCD_E_PIN, PinLow);		/* Enable is OFF */
+
+	_delay_ms(2);
+
+	/* send 4-LSB of Data */
+	Data = (cop_u8Data << 4) & 0xf0;
+	DIO_u8SetPortValue(LCD_DATAPORT, Data);
 	DIO_u8SetPinValue(LCD_CONTROLPORT, LCD_E_PIN, PinHigh);		/* Enable is ON */
 	_delay_ms(2);		/* Delay for 2 ms */
 	DIO_u8SetPinValue(LCD_CONTROLPORT, LCD_E_PIN, PinLow);		/* Enable is OFF */
 }
 
-/* write Command to LCD */
+/* Send Command to LCD */
 void LCD_vidSendCommand(u8 cop_u8Command){
-	
+	u8 Command = 0;
 	DIO_u8SetPinValue(LCD_CONTROLPORT, LCD_RS_PIN, PinLow); 	/* RS is OFF */
 	DIO_u8SetPinValue(LCD_CONTROLPORT, LCD_RW_PIN, PinLow);		/* RW is OFF */
-	DIO_u8SetPortValue(LCD_DATAPORT, cop_u8Command);			/* send Data */
+	/* send 4-MSB of Data */
+	Command = cop_u8Command & 0xf0;
+	DIO_u8SetPortValue(LCD_DATAPORT, Command);
 	DIO_u8SetPinValue(LCD_CONTROLPORT, LCD_E_PIN, PinHigh);		/* Enable is ON */
-	_delay_ms(2);		/* Delay for 1 ms */
+	_delay_ms(2);		/* Delay for 2 ms */
+	DIO_u8SetPinValue(LCD_CONTROLPORT, LCD_E_PIN, PinLow);		/* Enable is OFF */
+
+	_delay_ms(2);
+
+	/* send 4-LSB of Data */
+	Command = (cop_u8Command << 4) & 0xf0;
+	DIO_u8SetPortValue(LCD_DATAPORT, Command);
+	DIO_u8SetPinValue(LCD_CONTROLPORT, LCD_E_PIN, PinHigh);		/* Enable is ON */
+	_delay_ms(2);		/* Delay for 2 ms */
 	DIO_u8SetPinValue(LCD_CONTROLPORT, LCD_E_PIN, PinLow);		/* Enable is OFF */
 }
 
 /* LCD Initializiation */
 void LCD_vidInit (){
 	_delay_ms(40);
-	LCD_vidSendCommand(0b00111100);		/* Function set: 8-bit mode, 2-line display, 5x10 font */
+	LCD_vidSendCommand(0b00000010);		/* 4-bit mode */
+	LCD_vidSendCommand(0b00101100);		/* Function set: 4-bit mode, 2-line display, 5x10 font */
 	_delay_ms(1);
 	LCD_vidSendCommand(0b00001100);		/* Display ON, No Cursor */
 	_delay_ms(1);
@@ -41,6 +64,7 @@ void LCD_vidInit (){
 	LCD_vidSendCommand(0b00000110);		/* Entry Mode */
 
 }
+
 
 /* Set DDRAM Address */
 void LCD_vidSetDDRAMAddress(enum_row cop_enumrowNumber, enum_column cop_enumcolumnNumber){
